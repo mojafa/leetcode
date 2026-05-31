8277d01c053590890b8d3853ae8c9a8e700f9bb3
Jaafar <jmohsa@gmail.com>
2026-05-31 18:40:48 +0100

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n<=2){
+            return n;
+        }
+
+         int a = 1; // ways for step 1 behind (i-2)
+        int b = 2; // ways for step 1 behind (i-1)
+
+        for (int i =3; i<=n;i++){
+            int c = a+b;
+            a =b;
+            b=c;
+        }
+        return b;
+
+
+    }
+}
